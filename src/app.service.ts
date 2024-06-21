652077462a24b1a308840a41ba4c211780e4f82f
yogi <yogesh.nimbalkar07@gmail.com>
2024-06-21 12:45:49 +0530

    yogifinal

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! final';
+    return 'Hello World! final yogi';
   }
 
   getPing(): string {
